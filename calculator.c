7abacbe39165ffc3a8a3890522d35c77a350da1c
red12wings <72392454+red12wings@users.noreply.github.com>
2020-11-05 11:26:20 +0100

    Operatoren hinzugefügt, Datentypen geändert
    Operatoren - ; * ; /  bei der switch-case hinzugefügt
    int num2; auf float num2; geändert
    Beim Resultat in der printf von %f auf %.2f geändert damit es auf Kommazahlen reduziert ist

--- calculator.c
+++ calculator.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 /**
 * Simple calculator
@@ -6,26 +7,36 @@
 int main(){
 
 	float num1;
-	int num2;
+	float num2;
 	float ans;
 	char op;
 	printf("\nEnter an operator (+,-,/,*): \n");
 	scanf("%c",&op);
 
-	printf("\nEnter two numbers: ");
+	printf("\nEnter first numbers: ");
 	scanf("%f",&num1);
+	printf("\nEnter second numbers: ");
 	scanf("%f",&num2);
-	
+
 	switch(op){
-		case '+' : 
+		case '+' :
 			ans = num1 + num2;
+			break;
+        case '-' :
+			ans = num1 - num2;
+			break;
+        case '*' :
+			ans = num1 * num2;
+			break;
+        case '/' :
+			ans = num1 / num2;
 			break;
 		default:
 			printf("Enter a correct operator!");
 			return 1;
-	
+
 	}
 	printf("\n The result is as follows:\n");
-	printf("%f %c %f = %f",num1, op ,num2,ans);
+	printf("%.2f %c %.2f = %.2f",num1, op ,num2,ans);
 	return 0;
 }
